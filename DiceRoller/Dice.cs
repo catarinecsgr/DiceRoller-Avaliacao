917b5dde31589ec4bdcfd0329ec43c8b3114c5b3
catarinecsgr <catcas08@gmail.com>
2024-04-29 19:30:26 -0300

    avaliação finalizada

---
+++ DiceRoller/Dice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    class Dice
+    {
+        public Dice()
+        {
+
+        }
+
+        public Dice(int lados) {
+            Lados = lados;
+        }
+        // Atributos
+        private int Lados {  get; set; }
+
+        // Métodos
+        public int Rolar()
+        {
+            return new Random().Next(1, Lados+1);
+        }
+    }
+}
---
+++ DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DiceTypePicker.SelectedIndex = 0;
+        }
+
+        private void DiceBtn_Clicked(object sender, EventArgs e)
+        {
+            int sides = Convert.ToInt32(DiceTypePicker.SelectedItem);
+            // Instanciação de um novo objeto dice do tipo Dice através do contrutor Dice();
+            Dice dice = new Dice(sides);
+
+            DiceResult.Text = dice.Rolar().ToString();
+        }
+
+        private void DiceTypePicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DiceTypeImage.Source = $"d{DiceTypePicker.SelectedItem.ToString()}.png";
+            
+        }
+    }
+
+}
